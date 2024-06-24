54689d02f893b8ce3474961cb7c533ed0e5fa9d7
ABHISHEK RANJANAGOUDA G <101989040+abhiranjan2123@users.noreply.github.com>
2024-06-24 23:15:06 +0530

    Update RouteHandler.js

--- src/routes/RouteHandler.js
+++ src/routes/RouteHandler.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/loginPage";
 import RegisterPage from "../pages/RegisterPage";
 
